5e557b8e6473bf862115d2ed07faa951aac266af
vji-axelor <vji.axelor@gmail.com>
2021-06-13 20:39:49 +0530

    add container generation functionality

--- axelor-foods/src/main/java/com/axelor/apps/omfoods/web/ContainerController.java
+++ axelor-foods/src/main/java/com/axelor/apps/omfoods/web/ContainerController.java
@@ -1,13 +1,19 @@
 package com.axelor.apps.omfoods.web;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.axelor.apps.om.db.Container;
 import com.axelor.apps.om.db.ContainerProductLine;
+import com.axelor.apps.om.db.OmProduct;
+import com.axelor.apps.om.db.repo.OmProductRepository;
 import com.axelor.exception.AxelorException;
+import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.inject.Singleton;
-import java.math.BigDecimal;
-import java.util.List;
+import com.google.inject.persist.Transactional;
 
 @Singleton
 public class ContainerController {
@@ -32,4 +38,94 @@ public class ContainerController {
     response.setValue("totalProduct", prodCount);
     response.setValue("totalCost", totalCost);
   }
+  
+  @Transactional
+  public void addProductQty(ActionRequest request, ActionResponse response)
+	      throws AxelorException {
+	  Container container = request.getContext().asType(Container.class);
+	  OmProductRepository prodRepo = Beans.get(OmProductRepository.class);
+
+	    List<ContainerProductLine> containerProducts = new ArrayList<ContainerProductLine>();
+	    containerProducts = container.getProducts();
+
+	    for (ContainerProductLine containerProduct : containerProducts) {
+	      OmProduct product = containerProduct.getProduct();
+
+	      BigDecimal containerProductPiece = containerProduct.getQty();
+
+	      BigDecimal productPieceQty = product.getTotalPiece();
+	      BigDecimal productCartoonPiece = product.getUnitPiece();
+
+	      BigDecimal avlProductPieceQty = productPieceQty.add(containerProductPiece);
+	      BigDecimal avlProductCartoonQty = avlProductPieceQty.divide(productCartoonPiece);
+
+	      product.setTotalPiece(avlProductPieceQty);
+	      product.setUnitstock(avlProductCartoonQty);
+
+	      prodRepo.save(product);
+	    }
+  }
+  
+  @Transactional
+  public void removeSelectedProduct(ActionRequest request, ActionResponse response) throws AxelorException {
+	Container container = request.getContext().asType(Container.class);
+    OmProductRepository prodRepo = Beans.get(OmProductRepository.class);
+
+    List<ContainerProductLine> conProductLines = new ArrayList<ContainerProductLine>();
+    List<ContainerProductLine> updatedProductLines = new ArrayList<ContainerProductLine>();
+    conProductLines = container.getProducts();
+
+    for (ContainerProductLine conProductLine : conProductLines) {
+      if (conProductLine.isSelected()) {
+        OmProduct product = conProductLine.getProduct();
+
+        BigDecimal containerProdPiece = conProductLine.getQty();
+
+        BigDecimal productPieceQty = product.getTotalPiece();
+        BigDecimal productCartoonPiece = product.getUnitPiece();
+
+        BigDecimal avlProductPieceQty = productPieceQty.subtract(containerProdPiece);
+        BigDecimal avlProductCartoonQty = avlProductPieceQty.divide(productCartoonPiece);
+
+        product.setTotalPiece(avlProductPieceQty);
+        product.setUnitstock(avlProductCartoonQty);
+
+        prodRepo.save(product);
+      } else {
+    	  updatedProductLines.add(conProductLine);
+      }
+    }
+
+    response.setValue("products", updatedProductLines);
+  }
+  
+  @Transactional
+  public void resetProduct(ActionRequest request, ActionResponse response) throws AxelorException {
+
+	Container container = request.getContext().asType(Container.class);
+    OmProductRepository prodRepo = Beans.get(OmProductRepository.class);
+
+    List<ContainerProductLine> conProductLines = new ArrayList<ContainerProductLine>();
+    conProductLines = container.getProducts();
+
+    for (ContainerProductLine conProductLine : conProductLines) {
+    	OmProduct product = conProductLine.getProduct();
+
+        BigDecimal containerProdPiece = conProductLine.getQty();
+
+        BigDecimal productPieceQty = product.getTotalPiece();
+        BigDecimal productCartoonPiece = product.getUnitPiece();
+
+        BigDecimal avlProductPieceQty = productPieceQty.subtract(containerProdPiece);
+        BigDecimal avlProductCartoonQty = avlProductPieceQty.divide(productCartoonPiece);
+
+        product.setTotalPiece(avlProductPieceQty);
+        product.setUnitstock(avlProductCartoonQty);
+
+        prodRepo.save(product);
+    }
+
+    response.setValue("products", null);
+  }
+  
 }
